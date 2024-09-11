c3d8ba911c3196f78e449511b4ed82ba24bef611
Michael59158 <mwisniewski@ca-cf.Fr>
2024-09-11 16:31:51 +0200

    Create graph.py

---
+++ pages/graph.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import matpotlib as plt
+
+uploaded_file = st.file_uploader("Choose a file",file='csv')
+if uploaded_file is not None:
+    # To read file as bytes:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+    cols = dataframe.columns
+    user_select_cols = st.selectbox('Selectionnez les colonnes', cols)
+    #affichage de la colonne sélectionnée
+    st.write(dataframe[user_select_cols])
+
+    # graphique
+    cols_selected_value = dataframe[user_select_cols]
+    fig, ax = plt.subplots()
+    ax.hist(arr, bins=20)
+
+    st.pyplot(fig)
