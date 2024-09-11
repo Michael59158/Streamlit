af8b1ca6171eb097933dfbf0bcee4b537c6379df
Michael59158 <mwisniewski@ca-cf.Fr>
2024-09-11 14:59:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ st.title('My Dashboard MWI')
 # lecture du fichier et affectation dans un dataframe
 df = pd.read_csv('data.csv')
 
-# Envoi sur la sortie streamlit. 
-
-if st.checkbox('afficher le jeu de données'):
-  st.write(df)
-
 pro = df.Profession.unique()
 
 #liste déroulante
@@ -18,6 +13,10 @@ pro = df.Profession.unique()
 #liste déroulante sur la colonne Profession 
 user_selectbox_pro = st.selectbox('Selectionnez une profession :',pro)
 
-st.slider('Selectionner un age : ', min_value=18, max_value=99,value=30,step=1)
+# barre de sélection de min à max, avec position par défaut sur value et déplacement d'un pas de 1
+user_slider_age = st.slider('Selectionner un age : ', min_value=18, max_value=99,value=30,step=1)
 
-st.write(df[df.Profession == user_selectbox_pro])
+
+# Envoi sur la sortie streamlit. 
+if st.checkbox('afficher le jeu de données'):
+  st.write(df[(df.Profession == user_selectbox_pro)(df.Age == user_slider_age)])
