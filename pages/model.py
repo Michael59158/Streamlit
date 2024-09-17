edc473e5827452bc4f25cb0f964ef5e03e55fab9
Michael59158 <mwisniewski@ca-cf.Fr>
2024-09-17 15:51:42 +0200

    Update model.py

--- pages/model.py
+++ pages/model.py
@@ -1,5 +1,10 @@
 import streamlit as st 
 
+# barre de sélection de min à max, avec position par défaut sur value et déplacement d'un pas de 1
+user_slider_longitude = st.slider('longitude : ', min_value=-180, max_value=180,value=0,step=1)
+user_slider_latitude = st.slider('longitude : ', min_value=-180, max_value=180,value=0,step=1)
+
+
 with st.form("my_form"):
   st.write("My Form')
   name = st.text.input("Your name ?")
