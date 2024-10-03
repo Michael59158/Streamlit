32bba26d9b51b5d2d603f2691d153cff401400fe
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-03 16:19:06 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -6,11 +6,6 @@ st.title("FAQ Streamlit Michael")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     with st.chat_message("user"):
