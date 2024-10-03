5b6ecacbb36cbddb16028293f7c875411f135546
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-03 17:02:46 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.title("FAQ Streamlit Michael")
 
@@ -13,7 +14,18 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
+
+
+
+    url = "https://8a8d-34-148-30-210.ngrok-free.app/insert_data"
+    response_api = requests.get(url)
+
+    if response_api.status_code == 200:
+       response = f"Echo: {response_api.text}"
+    else:
+      response = f'Echo : "Error calling API:", response_api.status_code'
+
+    
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response)
