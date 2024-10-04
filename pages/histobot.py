ba049fbc6aba03be07a55f9d1c573cecaa6bf888
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-04 15:22:44 +0200

    Update histobot.py

--- pages/histobot.py
+++ pages/histobot.py
@@ -7,17 +7,14 @@ st.title('historique de conversation')
 col1,col2 = st.columns([1,2])
 col1.title('Sum:')
 
-with st.form('addition'):
-    a = st.number_input('a')
-    b = st.number_input('b')
-    submit = st.form_submit_button('add')
-
-
-#with st.form('submit'):
-#    base_url = st.text_input('Insert API URL')
-#    url = f"{base_url}select_data"
-#    select_url = f"{base_url}select_data"
-#    response_api_select = requests.get(select_url)
-#    df = pd.DataFrame(response_api_select.json())
-#    st.data_editor(df)
-#    submit = st.form_submit_button('submit')
+with st.form('exec'):
+    base_url = st.text_input('Insert API URL')
+    submit = st.form_submit_button('Submit)
+    
+if submit:    
+    url = f"{base_url}select_data"
+    select_url = f"{base_url}select_data"
+    response_api_select = requests.get(select_url)
+    df = pd.DataFrame(response_api_select.json())
+    st.data_editor(df)
+    
