13326368730f8acbea5b529bb38ed5465cfc7687
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-04 15:01:58 +0200

    Create homepage.py

---
+++ pages/homepage.py
@@ -0,0 +1,6 @@
+if st.button("Home"):
+    st.switch_page("pages/homepage.py")
+if st.button("Conversation avec l'assistant"):
+    st.switch_page("pages/chatbot.py")
+if st.button("Historique de conversation"):
+    st.switch_page("pages/histobot.py")
