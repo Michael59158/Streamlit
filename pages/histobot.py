727f63c47eb667bac049184e32d29264cd2335fe
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-04 14:57:32 +0200

    Update histobot.py

--- pages/histobot.py
+++ pages/histobot.py
@@ -1,7 +1,11 @@
-left, right = st.columns(2)
-if left.button("Historique de conversation", use_container_width=True):
-    url = f"{base_url}select_data"
-    select_url = f"{base_url}select_data"
-    response_api_select = requests.get(select_url)
-    df = pd.DataFrame(response_api_select.json())
-    st.data_editor(df)
+import streamlit as st
+import requests
+import pandas as pd 
+
+st.title.write('historique de conversation')
+
+url = f"{base_url}select_data"
+select_url = f"{base_url}select_data"
+response_api_select = requests.get(select_url)
+df = pd.DataFrame(response_api_select.json())
+st.data_editor(df)
