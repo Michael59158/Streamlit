b1a2995ff9711eebeb68048eea5dacc77b3120a7
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-04 09:18:44 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -18,7 +18,9 @@ if prompt := st.chat_input("What is up?"):
 
     url = "https://1cdb-34-31-223-192.ngrok-free.app/insert_data"
     data = {"message": prompt}
-    print(data)
+    with st.echo():
+        st.write('data',data)
+ 
     response_api = requests.post(url,json=data)
 
     if response_api.status_code == 200:
