a97a30d6fb8e5d2a3633fd0a9122ec4c2951a5c7
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-04 14:41:45 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -42,13 +42,12 @@ if right.button("Conversation", icon=":material/mood:", use_container_width=True
            response = f"{response_api.text}"
         else:
           response = f'Error calling API:, {response_api.status_code}'
-    
-        
+            
         # Display assistant response in chat message container
-     with st.chat_message("assistant"):
-          st.markdown(response)
-     # Add assistant response to chat history
-     st.session_state.messages.append({"role": "assistant", "content": response})
+         with st.chat_message("assistant"):
+              st.markdown(response)
+         # Add assistant response to chat history
+         st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 
