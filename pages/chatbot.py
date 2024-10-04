54bb08737d581c1a864c4fe2efec52141e0bccef
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-04 14:40:51 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -21,11 +21,10 @@ if left.button("Historique de conversation", use_container_width=True):
  
 
 if right.button("Conversation", icon=":material/mood:", use_container_width=True):
-# Initialize chat history
-    if "messages" not in st.session_state:
+
+   if "messages" not in st.session_state:
         st.session_state.messages = []
-    
-    if prompt := st.chat_input("What is up?"):
+   if prompt := st.chat_input("What is up?"):
         # Display user message in chat message container
         with st.chat_message("user"):
              st.markdown(prompt)
@@ -42,14 +41,14 @@ if right.button("Conversation", icon=":material/mood:", use_container_width=True
         if response_api.status_code == 200:
            response = f"{response_api.text}"
         else:
-          response = f'Echo : "Error calling API:", {response_api.status_code}'
+          response = f'Error calling API:, {response_api.status_code}'
     
         
         # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown(response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+     with st.chat_message("assistant"):
+          st.markdown(response)
+     # Add assistant response to chat history
+     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 
