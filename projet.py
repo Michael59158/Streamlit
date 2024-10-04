09b81d5edb4119daf8451165f5525d732504f704
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-04 15:02:13 +0200

    Delete projet.py

--- projet.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-if st.button("Home"):
-    st.switch_page("projet.py")
-if st.button("Conversation avec l'assistant"):
-    st.switch_page("chatbot.py")
-if st.button("Historique de conversation"):
-    st.switch_page("histobot.py")
