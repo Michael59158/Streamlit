5bf8260f5f606d1ad27ca84918e287d6b52f1a80
Michael59158 <mwisniewski@ca-cf.Fr>
2024-10-04 15:16:19 +0200

    Update histobot.py

--- pages/histobot.py
+++ pages/histobot.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 st.title('historique de conversation')
 
-base_url = st.text_input("Insert API URL")
-
-url = f"{base_url}select_data"
-select_url = f"{base_url}select_data"
-response_api_select = requests.get(select_url)
-df = pd.DataFrame(response_api_select.json())
-st.data_editor(df)
+with st.form('Submit'):
+    base_url = st.text_input('Insert API URL')
+    url = f"{base_url}select_data"
+    select_url = f"{base_url}select_data"
+    response_api_select = requests.get(select_url)
+    df = pd.DataFrame(response_api_select.json())
+    st.data_editor(df)
